03a5c7f5b9ebb6f2d5eb1bea64547e76bce35ef4
pk0205 <kumarprashant670@gmail.com>
2021-02-07 00:35:25 +0530

    Before paypal Integration

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import { protect } from './../middleware/authMiddleware.js';
+import {
+  addOrderItems,
+  getOrderByID,
+  updateOrderToPaid,
+} from './../controllers/orderController.js';
+
+const router = express.Router();
+
+router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderByID);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+
+export default router;
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -14,11 +14,6 @@ const RegisterScreen = ({ location, history }) => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState(null);
 
-  const [nameErr, setNameErr] = useState('');
-  const [emailErr, setEmailErr] = useState('');
-  const [passwordErr, setPasswordErr] = useState('');
-  const [confirmPasswordErr, setConfirmPasswordErr] = useState('');
-
   const dispatch = useDispatch();
 
   const userRegister = useSelector((state) => state.userRegister);
@@ -34,29 +29,7 @@ const RegisterScreen = ({ location, history }) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if (!name) {
-      setNameErr('name is required');
-    } else {
-      setNameErr('');
-    }
-    if (!email) {
-      setEmailErr('email is required');
-    } else {
-      setEmailErr('');
-    }
-    if (!password) {
-      setPasswordErr('password is required');
-    } else {
-      setPasswordErr('');
-    }
-    if (!confirmPassword) {
-      setConfirmPasswordErr('password is required');
-    } else {
-      setConfirmPasswordErr('');
-    }
-    if (!name || !email || !password || !confirmPassword) {
-      return;
-    } else if (password !== confirmPassword) {
+    if (password !== confirmPassword) {
       setMessage('Passwords do not match');
     } else {
       setMessage('');
@@ -75,47 +48,45 @@ const RegisterScreen = ({ location, history }) => {
           <Form.Label>Name</Form.Label>
           <Form.Control
             type="name"
+            required
             placeholder="Enter name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        {nameErr && <Message variant="danger">{nameErr}</Message>}
 
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type="email"
+            required
             placeholder="Enter email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        {emailErr && <Message variant="danger">{emailErr}</Message>}
 
         <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
+            required
             placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        {passwordErr && <Message variant="danger">{passwordErr}</Message>}
 
         <Form.Group controlId="confirmPassword">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
             type="password"
+            required
             placeholder="Confirm password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        {confirmPasswordErr && (
-          <Message variant="danger">{confirmPasswordErr}</Message>
-        )}
 
         <Button type="submit" variant="primary">
           Register
