9095c21324c7292ba2816f788e0b6f34a9ab3a6e
pk0205 <kumarprashant670@gmail.com>
2021-02-08 13:05:30 +0530

    deploying v1

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; proShop</Col>
+          <Col className="text-center py-3">Copyright &copy; MERNSHOP</Col>
         </Row>
       </Container>
     </footer>
